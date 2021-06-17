adc0790aabf64e49927317ee537a5dbac382f326
SBasanko <83013206+SBasanko@users.noreply.github.com>
2021-06-17 20:05:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
 
-	let items = [{
-		name: 'A',
-		score: 0
-	}];
+	let items = [];
 
 	$('.add-item').on('click', function () {
 		const inputVal = $('.item-input').val();
+		if (!inputVal.length) {
+			return;
+		}
 		items.push(newItem(inputVal));
 		renderTable();
+		renderBtns();
 	});
 
-	function newItem(val) {
+
+
+
+
+	function newItem(val) { // val = z
 		return {
-			name: val,
+			name: val, //z
 			score: 0
 		}
 	}
 
 	function renderTable() {
+		$('.t-body').empty();
 		$.each(items, function (i, item) {
-			console.log(item)
+
 			$('.t-body').append(createTableItem(item))
 		});
 	}
@@ -29,6 +35,46 @@ $(document).ready(function () {
 
 	function createTableItem(item) {
 		return '<div class="item"><div>' + item.name + '</div> <div>' + item.score + '</div></div> ';
+	}
+
+
+	$('.radio-selects input').on('click', function (event) {
+		event.stopPropagation();
+		//console.log(event);
+
+	});
+
+	function renderBtns() {
+		if (items.length < 2) {
+			return
+		}
+		$('.compare-block').empty();
+		$.each(items, function (i, item) {
+
+			if (i + 1 == items.length) {
+				return;
+			}
+
+			$.each(items, function (j) {
+				if (i >= j) {
+					return;
+				} else {
+					$('.compare-block').append(createCompareItem(item, items[j]))
+				}
+
+
+			})
+
+
+
+		});
+
+
+
+	}
+
+	function createCompareItem(firstEl, secondEl) {
+		return '<div class="case"><button>' + firstEl.name + '</button> <button>' + secondEl.name + '</button></div> ';
 	}
 
 
